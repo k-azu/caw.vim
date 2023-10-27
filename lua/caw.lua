3c27a076b4beee7bcad5bc0e2c48c7277c100f7e
k-azu <49285910+k-azu@users.noreply.github.com>
2023-10-28 00:43:06 +0900

    Update

--- lua/caw.lua
+++ lua/caw.lua
@@ -9,9 +9,35 @@ local M = {}
 
 function M.has_syntax(lnum, col)
   local bufnr = vim.api.nvim_get_current_buf()
-  for _, capture in pairs(vim.treesitter.get_captures_at_pos(bufnr, lnum - 1, col - 1)) do
-    for _, cap in pairs(capture) do
-      if string.match(cap, 'comment') then
+  -- vim.treesitter.highlighter.hl_map was removed with Neovim 0.8.0 release.
+  -- see: https://github.com/neovim/neovim/pull/19931
+  if vim.fn.has("nvim-0.5.0") == 1 and vim.fn.has("nvim-0.8.0") == 0 then
+    local col = col - 1
+    local filetype = vim.api.nvim_buf_get_option(bufnr, "ft")
+    local lang = languages[filetype] or filetype
+    if not require("vim.treesitter.language").require_language(lang, nil, true) then
+      return false
+    end
+    local query = require("vim.treesitter.query").get_query(lang, "highlights")
+    local tstree = vim.treesitter.get_parser(bufnr, lang):parse()[1]
+    local tsnode = tstree:root()
+
+    for _, match in query:iter_matches(tsnode, bufnr, lnum - 1, lnum) do
+      for id, node in pairs(match) do
+        local _, start_col, _, end_col = node:range()
+        local name = query.captures[id]
+        local highlight = vim.treesitter.highlighter.hl_map[name]
+
+        if col >= start_col and col < end_col and string.match(highlight, "Comment") then
+          return true
+        end
+      end
+    end
+  elseif vim.fn.has("nvim-0.8.0") == 1 then
+    -- vim.treesitter.get_captures_at_pos using 0-based coordinate
+    local captures = vim.treesitter.get_captures_at_pos(bufnr, lnum - 1, col - 1)
+    for _, capture in ipairs(captures) do
+      if capture.capture == "comment" then
         return true
       end
     end
@@ -19,31 +45,5 @@ function M.has_syntax(lnum, col)
 
   return false
 end
--- function M.has_syntax(lnum, col)
---   local col = col - 1
---   local bufnr = vim.api.nvim_get_current_buf()
---   local filetype = vim.api.nvim_buf_get_option(bufnr, 'ft')
---   local lang = languages[filetype] or filetype
---   if not require"vim.treesitter.language".require_language(lang, nil, true) then
---     return false
---   end
---   local query = require"vim.treesitter.query".get_query(lang, "highlights")
---   local tstree = vim.treesitter.get_parser(bufnr, lang):parse()[1]
---   local tsnode = tstree:root()
-
---   for _, match in query:iter_matches(tsnode, bufnr, lnum - 1, lnum) do
---     for id, node in pairs(match) do
---       local _, start_col, _, end_col = node:range()
---       local name = query.captures[id]
---       local highlight = vim.treesitter.highlighter.hl_map[name] or ''
-
---       if col >= start_col and col < end_col and string.match(highlight, 'Comment') then
---         return true
---       end
---     end
---   end
-
---   return false
--- end
 
 return M
